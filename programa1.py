bc5c66d5913009f6f125cb4156f19aedb499410f
Gonzalo Zabala <gzabala@gmail.com>
2020-10-26 17:37:11 -0300

    Le agregué mi comentario

--- programa1.py
+++ programa1.py
@@ -1,3 +1,4 @@
 Esto lo escribió Gonzalo el 16 10 a las 18:25
 Y esto lo escribió GZ el 16 10 a las 18:32
 Esto es una prueba para ver si logro hacer push. Damian
+Esta es una modificación de Gonzalo del 26/10 a las 17:36
